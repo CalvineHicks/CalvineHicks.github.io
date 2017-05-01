f291278a7bc2080ad924086cbb22a69045c1a537
Thurman and Rachel <thurman.justice@gmail.com>
2017-04-30 18:20:04 -0600

    add advanced search page to display search results

--- app/public/js/controller/searchController.js
+++ app/public/js/controller/searchController.js
@@ -3,7 +3,11 @@ app.controller('SearchController', function SearchController($scope, $http) {
 
   $scope.advancedSearchInput = '';
 
-  $scope.advancedSearch = function(){
+  $scope.advancedSearch = debounce(function(){
+    $scope.ebayResults = [];
+    $scope.craigslistResults = [];
+    $scope.results = [];
+    $scope.results.length = 0;
     $http({
       method: 'GET',
       headers: {
@@ -11,11 +15,40 @@ app.controller('SearchController', function SearchController($scope, $http) {
       },
       url: 'http://localhost:8080/ebay/?queryString='+$scope.advancedSearchInput
     }).then(function successCallback(response) {
-        console.log(response);
-        console.log('test');
+        $scope.ebayResults = response['data'];
+        $scope.results = $scope.results.concat(response['data']);
       }, function errorCallback(response) {
         console.log('error');
         console.log(response);
       });
-  };
+
+    $http({
+      method: 'GET',
+      headers: {
+        'Content-Type' : 'text/html'
+      },
+      url: 'http://localhost:8080/craigslist/?city=denver&queryString='+$scope.advancedSearchInput
+      }).then(function successCallback(response) {
+        $scope.craigslistResults = response['data'];
+        $scope.results = $scope.results.concat(response['data']);
+      }, function errorCallback(response) {
+        console.log('error');
+        console.log(response);
+      });
+  }, 1000, false);
 });
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
--- app/services/craigslist.js
+++ app/services/craigslist.js
@@ -5,6 +5,7 @@ var xpath = require('xpath');  //xpath parser
 var dom = require('xmldom').DOMParser; //dom obj for xpath
 
 router.get('/craigslist', function(req, res) {
+console.log('getting craigslist data');
 var city = req.query.city.toUpperCase();
 var queryString = req.query.queryString;
 request('http://'+city+'.craigslist.org/search/sss?format=rss&query='+queryString+'', function (error, response, body) {
@@ -53,7 +54,7 @@ request('http://'+city+'.craigslist.org/search/sss?format=rss&query='+queryStrin
         responseModels.push(responseModel);
         }
 
-        res.json({"response" : responseModels})
+        res.json(responseModels)
      }
 })
 });
