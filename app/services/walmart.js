e5e7489c3b0897c2830fe827d72c588e809d3a03
Thurman and Rachel <thurman.justice@gmail.com>
2017-08-13 17:34:28 -0600

    removed walmart category filter

--- app/services/walmart.js
+++ app/services/walmart.js
@@ -7,7 +7,7 @@ router.get('/walmart', function(req, res) {
     var queryString = req.query.queryString;
     var sort = req.query.sort;
     var order = req.query.order;
-    var endpoint = 'http://api.walmartlabs.com/v1/search?apiKey='+properties.walmart.app_key+'&query='+queryString+'&categoryId=+976760_1005860_1224972';
+    var endpoint = 'http://api.walmartlabs.com/v1/search?apiKey='+properties.walmart.app_key+'&query='+queryString;
     var pageNum = req.query.pageNum;
     if(sort){
         endpoint = endpoint + '&sort='+sort+'&order='+order;
